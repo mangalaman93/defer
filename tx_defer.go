e467dc473ad5aa468872e5b79577f8f1ea584388
Aman Mangal <aman.mangal@zeotap.com>
2018-04-21 15:04:38 +0530

    Add example of rollback using defer

---
+++ tx_defer.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/coreos/etcd/client"
+)
+
+const (
+	cCinderConfFilePath = "priv/cinder/nimble.conf"
+	cEtcdKey            = "/cinder/storage/nimble_onprem"
+
+	cCinderConfForNimble = "{\"san_ip\":\"10.23.0.1\",\"san_login\":\"nimble\",\"san_password\":\"nimble123\",\"volume_backend_name\":\"nimble_onprem\"}"
+)
+
+var (
+	ErrRestartingCinder = errors.New("failed to restart cinder")
+	ErrWritingToFile    = errors.New("failed to write to cinder.conf")
+	randSource          = rand.New(rand.NewSource(time.Now().UnixNano()))
+)
+
+func restartCinderService() error {
+	//if randSource.Intn(100) < 100 {
+	//	return ErrRestartingCinder
+	//}
+
+	log.Println("cinder service restarted")
+	return nil
+}
+
+func writeToFile(filepath string, data []byte) error {
+	if randSource.Intn(100) < 101 {
+		return ErrWritingToFile
+	}
+
+	err := ioutil.WriteFile(filepath, data, os.ModePerm)
+	if err != nil {
+		log.Println("failed to write to file")
+		return err
+	}
+
+	log.Println("writing to file complete")
+	return nil
+}
+
+func deleteFile(filepath string) error {
+	if err := os.Remove(filepath); err != nil {
+		log.Println("error in deleting file")
+		return err
+	}
+
+	log.Println("deleting file complete")
+	return nil
+}
+
+func step1(api client.KeysAPI) (*client.Response, error) {
+	resp, err := api.Set(context.Background(), cEtcdKey, cCinderConfForNimble, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("Set is done. Metadata is %q\n", resp)
+	return resp, nil
+}
+
+func undoStep1(api client.KeysAPI, resp *client.Response) {
+	if resp.PrevNode == nil {
+		_, err := api.Delete(context.Background(), cEtcdKey, nil)
+		if err != nil {
+			log.Printf("unable to delete key:%v :: %v\n", cEtcdKey, err)
+		}
+		log.Printf("deleted key: %v\n", cEtcdKey)
+	} else {
+		_, err := api.Set(context.Background(), cEtcdKey, resp.PrevNode.Value, nil)
+		if err != nil {
+			log.Printf("unable to undo set for key:%v, value:%v :: %v\n", cEtcdKey, resp.PrevNode.Value, err)
+		}
+		log.Printf("undo set value for key: %v\n", cEtcdKey)
+	}
+}
+
+func performTransaction() error {
+	// BEGIN transaction
+	success := false
+
+	// ################## STEP 1 ##################
+	// setup client to etcd
+	cfg := client.Config{
+		Endpoints: []string{"http://127.0.0.1:2379"},
+	}
+	c, err := client.New(cfg)
+	if err != nil {
+		return err
+	}
+	keysAPI := client.NewKeysAPI(c)
+	resp, err := step1(keysAPI)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if !success {
+			undoStep1(keysAPI, resp)
+		}
+	}()
+
+	// ################## STEP 2 ##################
+	if err := writeToFile(cCinderConfFilePath, []byte(cCinderConfForNimble)); err != nil {
+		return err
+	}
+
+	defer func() {
+		if !success {
+			deleteFile(cCinderConfFilePath)
+		}
+	}()
+
+	// ################## STEP 3 ##################
+	if err := restartCinderService(); err != nil {
+		return err
+	}
+
+	success = true
+	return nil
+	// END transaction
+}
+
+func main() {
+	if err := performTransaction(); err != nil {
+		log.Println("transaction failed ::", err)
+	} else {
+		log.Println("transaction complete")
+	}
+}
---
+++ tx_notx.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/coreos/etcd/client"
+)
+
+const (
+	cCinderConfForNimble = "{\"san_ip\":\"10.23.0.1\",\"san_login\":\"nimble\",\"san_password\":\"nimble123\",\"volume_backend_name\":\"nimble_onprem\"}"
+)
+
+var (
+	ErrRestartingCinder = errors.New("failed to restart cinder")
+	randSource          = rand.New(rand.NewSource(time.Now().UnixNano()))
+)
+
+func writeToFile(filepath string, data []byte) error {
+	//if randSource.Intn(100) < 101 {
+	//	return ErrWritingToFile
+	//}
+
+	err := ioutil.WriteFile(filepath, data, os.ModePerm)
+	if err != nil {
+		log.Println("failed to write to file")
+		return err
+	}
+
+	log.Println("writing to file complete")
+	return nil
+}
+
+func restartCinderService() error {
+	//if randSource.Intn(100) < 50 {
+	//	return ErrRestartingCinder
+	//}
+
+	log.Println("cinder service restarted")
+	return nil
+}
+
+func main() {
+	// BEGIN transaction
+
+	// ################## STEP 1 ##################
+	// setup client to etcd
+	cfg := client.Config{
+		Endpoints: []string{"http://127.0.0.1:2379"},
+	}
+	c, err := client.New(cfg)
+	if err != nil {
+		panic(err)
+	}
+	keysAPI := client.NewKeysAPI(c)
+
+	// set the key
+	resp, err := keysAPI.Set(context.Background(), "/cinder/storage/nimble_onprem",
+		cCinderConfForNimble, nil)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("Set is done. Metadata is %q\n", resp)
+
+	// ################## STEP 2 ##################
+	if err := writeToFile("priv/cinder/cinder.conf",
+		[]byte(cCinderConfForNimble)); err != nil {
+
+		panic(err)
+	}
+
+	// ################## STEP 3 ##################
+	if err := restartCinderService(); err != nil {
+		panic(err)
+	}
+
+	// END transaction
+	log.Println("transaction complete")
+}
